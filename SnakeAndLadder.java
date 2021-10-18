40524f27a4e41bbdcd8c622e5b64a9150d6f8529
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-10-19 01:05:41 +0530

    Ensuring exact 100 winning position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,27 +2,31 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
-		System.out.println("Player Winning Position is: ");
+		System.out.println("Player Exact Winning Position is: ");
 
-	
-		// Initialize Variable
-		
-		int position = 0; 
+//		constants for program 
+		int WINNING_POSITION = 100;
+		int position = 0; // Initializing Variable
 
-		while (position < 100) {
+		// while loop use to execute Condition
+		while (position < WINNING_POSITION) {
 
 			// to roll a dice
 			int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
 			System.out.println("diceNumber is : " + diceNumber);
 
 			// check option after getting a diceNumber
-			int option = (int) Math.floor(Math.random() * 10) % 3;
+			int option = (int) Math.floor(Math.random() * 10) % 2;
 			System.out.println("selected player option : " + option);
 
 			switch (option) {
 			case 1:
 				position = diceNumber + position;
 				System.out.println("Ladder");
+				if (position > 100) {
+					position = 100;
+					position = position - diceNumber;
+				}
 				System.out.println("position for the player after the ladder is : " + position);
 				break;
 
