2c717128b32a69732a98103c6d88dffd72b05586
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-10-19 00:08:40 +0530

    game starts with Zero position

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		
+		// starting position of player
+		int startPosition = 0;
+		System.out.println(" start position is: " + startPosition);
+	}
+}
